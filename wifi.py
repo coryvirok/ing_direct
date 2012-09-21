30056a1477363656513ca4f84bb4cbd3991254e8
Cory Virok <coryvirok@gmail.com>
2012-09-20 17:05:02 -0700

    better to run with watch -n30 instead of sleeping... I think there's some cookie caching somewhere

--- wifi.py
+++ wifi.py
@@ -9,20 +9,17 @@ form_password_re = re.compile(r'name="password" value="([^"]+)"')
 if __name__ == '__main__':
     user = sys.argv[1]
     action = 'http://10.0.4.13/ING_Cafe.php'
-    while True:
-        print 'signing in'
-        resp = requests.post(action, {'user': user})
+    print 'signing in'
+    resp = requests.post(action, {'user': user})
 
-        action_match = form_action_re.search(resp.text)
-        pw_match = form_password_re.search(resp.text)
+    action_match = form_action_re.search(resp.text)
+    pw_match = form_password_re.search(resp.text)
 
-        if action_match and pw_match:
-            action = action_match.group(1)
-            pw = pw_match.group(1)
+    if action_match and pw_match:
+        action = action_match.group(1)
+        pw = pw_match.group(1)
 
-            requests.post(action, {'user': user, 'password': pw, 'cmd': 'authenticate', 'Login': '"Log In"'})
-            print 'signed in'
-        else:
-            print 'already signed in'
-
-        time.sleep(30)
+        requests.post(action, {'user': user, 'password': pw, 'cmd': 'authenticate', 'Login': '"Log In"'})
+        print 'signed in'
+    else:
+        print 'already signed in'
